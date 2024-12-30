ca488b9331b59b75c755343e24ffee303f5e9fe8
Blackeye994 <msamad09874821@gmail.com>
2024-12-30 10:47:52 +0500

    Add New files

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ if(process.env.NODE_ENV != "production"){
 }
 
 const express = require("express");
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 
 const SignUpRoutes = require("./Route/SignUp.js");
